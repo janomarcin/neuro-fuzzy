bfd2cc143142256815b9d923e008e138706f97ac
RomanMichna <roman.michna@student.tuke.sk>
2012-11-08 15:27:50 +0100

    Tak a je to :P

---
+++ controllers/BRobot/BRobot/BRobot.cpp
@@ -0,0 +1,44 @@
+#include <webots/Robot.hpp>
+#include <webots/LED.hpp>
+#include <webots/DistanceSensor.hpp>
+
+#include <iostream>
+
+using namespace webots;
+
+#define TIME_STEP 32
+
+class MyRobot : public Robot 
+{
+private:
+	//LED *led;
+	DistanceSensor *distanceSensor;
+
+public:
+	MyRobot() : Robot() {
+		//led = getLED("ledName");
+		
+		distanceSensor = getDistanceSensor("ps1");
+		distanceSensor->enable(TIME_STEP);
+		}
+
+	virtual ~MyRobot() { }
+
+	void run() 
+	{
+		while (step(TIME_STEP) != -1) 
+		{
+			double val = distanceSensor->getValue();
+			std::cout << val << std::endl;
+		}
+	}
+};
+
+
+int main(int argc, char **argv)
+{
+  MyRobot* controller = new MyRobot();
+  controller->run();
+  delete controller;
+  return 0;
+}
